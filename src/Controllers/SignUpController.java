880c5279860a73d7a9b2a5d0759b11d9badd9db1
Shimon Metzger <62279263+timond44@users.noreply.github.com>
2020-12-17 01:58:42 +0200

    שגיאת קומבו

--- src/Controllers/SignUpController.java
+++ src/Controllers/SignUpController.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
+import Exceptions.comboBoxNotSelected;
 import Model.Gender;
 import Model.Language;
 import Model.LocalGuide;
@@ -48,7 +49,7 @@ public class SignUpController {
     private Button btnSignIn;
 
     @FXML
-    private ComboBox<?> comBoxUserType;
+    private ComboBox<String> comBoxUserType;
 
     @FXML
     private Label lblUserType;
@@ -155,7 +156,10 @@ public class SignUpController {
     @FXML
     private Label lblLogo;
     SystemGuide4u system= SystemGuide4u.getInstance();
-
+    @FXML
+    public void initialize() {
+    	comBoxUserType.getItems().setAll("Local Guide", "Traveller", "Traveller and Local Guide");
+    }
     @FXML
     void btnExitClick(ActionEvent event) {
     	System.exit(0);
@@ -163,25 +167,37 @@ public class SignUpController {
     }
 
     @FXML
-    void btnSignInClick(ActionEvent event) {
+    void btnSignInClick(ActionEvent event) throws comboBoxNotSelected {
     	if((system.checkPassword(txtPassword)) 
     	&& (system.checkValidateEmail(txtEmail.getText()))
     	&& (system.checkFirstName(txtFirstName.getText()))){
+    		
 		    	LocalDate localDate = comBoxDOB.getValue();
 		    	Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
 		    	Date date = (Date) Date.from(instant);
+		    	Language language;
+		    	TravelStyle travelStyle;
 		    	boolean emailNotes=false;
 		    	if(checkBoxEmailNots.equals(true))
 		    		emailNotes=true;
 		    	//String date2 = comBoxDOB.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-			    Language language=new Language(comBoxLang1.getValue().toString());
-			    TravelStyle travelStyle= new TravelStyle(comBoxTravelStyle1.getValue().toString());
+		    	
+		    	if(comBoxLang1.getValue()!=null) {
+				     language=new Language(comBoxLang1.getValue().toString());
+		    	}
+		    	else throw new comboBoxNotSelected();
+		    	
+		    	if(comBoxTravelStyle1.getValue()!=null) {
+				    travelStyle= new TravelStyle(comBoxTravelStyle1.getValue().toString());
+		    	}
+		    	else throw new comboBoxNotSelected();
+		    	
 			    Gender gender= Gender.Female;
 			    if(comBoxGender.getValue().equals("Male"))
 			    	gender=Gender.Male;
 			    
-		    	if(comBoxUserType.getValue().equals("Local guide")) {
-					 LocalGuide localGuide = new LocalGuide(txtEmail.getText(),
+		    	
+				LocalGuide localGuide = new LocalGuide(txtEmail.getText(),
 							 txtPassword.getText(),
 							 txtFirstName.getText(), 
 							 txtLastName.getText(),
@@ -194,30 +210,33 @@ public class SignUpController {
 							 travelStyle, 
 							 txtAboutMe.getText(), 
 							 emailNotes);
-			
+				Traveller traveller= new Traveller(txtEmail.getText(),
+						 txtPassword.getText(),
+						 txtFirstName.getText(), 
+						 txtLastName.getText(),
+						 date, 
+						 gender,
+						 txtCity.getText(),
+						 comBoxCountry.getValue().toString(),
+						 Integer.parseInt(txtPhone.getText()), 
+						 language,
+						 travelStyle, 
+						 txtAboutMe.getText(), 
+						 emailNotes);
+		    	
+				if(comBoxUserType.getValue().equals("Local Guide")) 
 					 system.addGuide(localGuide);
-		
-		    	}
-		    	else {
-		    		Traveller traveller= new Traveller(txtEmail.getText(),
-							 txtPassword.getText(),
-							 txtFirstName.getText(), 
-							 txtLastName.getText(),
-							 date, 
-							 gender,
-							 txtCity.getText(),
-							 comBoxCountry.getValue().toString(),
-							 Integer.parseInt(txtPhone.getText()), 
-							 language,
-							 travelStyle, 
-							 txtAboutMe.getText(), 
-							 emailNotes);
+	
+		    	if(comBoxUserType.getValue().equals("Traveller"))
 			       system.addTraveller(traveller);
-		    		
-		    	}
-    }
-    }
+		    	if(comBoxUserType.getValue().equals("Traveller and Local Guide"))	
+		    	   system.addGuide(localGuide);system.addTraveller(traveller);}
+		    	else throw new comboBoxNotSelected();////צריך להחליט מה עושים פה
+    	
+    	    
+		
+    	
     }
 
-
+}
 
