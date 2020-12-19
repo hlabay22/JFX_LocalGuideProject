218dabf640a46e84c6d551bc18280f6206d0566c
Shimon Metzger <62279263+timond44@users.noreply.github.com>
2020-12-19 23:59:40 +0200

    Add files via upload

---
+++ src/Controllers/PopUpLoginErrorController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PopUpLoginErrorController {
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    private Button btnOK;
+
+    @FXML
+    void btnOKclick(ActionEvent event) {
+    	btnOK.getScene().getWindow().hide();
+
+    }
+
+}
+
